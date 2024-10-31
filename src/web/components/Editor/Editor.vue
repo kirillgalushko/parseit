714880ea914605c185f29f1c81df5049a62676e4
Kirill Galushko <yoursgk@mail.ru>
2024-10-31 18:50:45 +0600

    Add russian language to editor

--- src/web/components/Editor/Editor.vue
+++ src/web/components/Editor/Editor.vue
@@ -4,6 +4,7 @@ import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ru-ru'
 import { onMounted, ref, watch } from 'vue';
 import { useArticleStore } from '../../stores/articleStore';
 import { Article } from '../../types/Article';
@@ -37,6 +38,7 @@ const createEditor = () => {
       target: "_blank",
       rel: "noopener noreferrer",
     },
+    language: 'ru-RU',
     frontMatter: true,
     initialValue: props.article.markdown,
     usageStatistics: false,
--- src/web/components/Editor/Viewer.vue
+++ src/web/components/Editor/Viewer.vue
@@ -4,6 +4,7 @@ import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight-all.js';
 import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor/dist/i18n/ru-ru'
 import { onMounted, ref, watch } from 'vue';
 import { Article } from '../../types/Article';
 import './styles.css'
@@ -26,6 +27,7 @@ const createViewer = () => {
       target: "_blank",
       rel: "noopener noreferrer",
     },
+    language: 'ru-RU',
     frontMatter: true,
     initialValue: props.article.markdown,
     usageStatistics: false,
