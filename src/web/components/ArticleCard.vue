6387d752d565793d108e44d944e13acc8ec90583
Kirill Galushko <yoursgk@mail.ru>
2024-11-02 20:47:22 +0600

    Add article card context menu

--- src/web/components/ArticleCard.vue
+++ src/web/components/ArticleCard.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { Text, Stack, Card, Gap } from 'gui'
+import { Text, Stack, Card, Gap, useContextMenu } from 'gui'
 import { storeToRefs } from 'pinia'
+import ArticleCardContextMenu from 'src/web/components/ArticleCardContextMenu.vue'
 import Favicon from 'src/web/components/Favicon.vue'
 import HighlightText from 'src/web/components/HighlightText.vue'
 import { useArticleStore } from 'src/web/stores/articleStore'
 import { useSearchStore } from 'src/web/stores/searchStore'
 import { Article } from 'src/web/types/Article'
 import { trimAroundSubstring } from 'src/web/utils/trimAroundSubstring'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 interface ArticleCardProps {
   article: Article
@@ -20,24 +21,28 @@ const { searchQuery } = storeToRefs(useSearchStore())
 const searchResultText = computed(() => {
   return trimAroundSubstring(props.article.body, searchQuery.value, 2, 15)
 })
+const { contextMenuData, targetRef } = useContextMenu()
 </script>
 
 <template>
-  <Card
-    :background="articleStore.selectedArticle?.id === props.article.id ? 'secondary' : 'default'"
-    interactive
-    stretched
-    :onClick="props.onClick"
-  >
-    <Text ellipsis typography="title-4-semibold">{{ article.name }}</Text>
-    <Gap direction="vertical" :default="1" />
-    <Text ellipsis :clamp="2" typography="paragraph-3-regular">
-      <HighlightText :text="searchResultText || article.excerpt" :searchQuery="searchQuery" />
-    </Text>
-    <Gap direction="vertical" :default="2" />
-    <Stack direction="row" :gap="2" stretched>
-      <Favicon v-if="article.faviconUrl" :src="article.faviconUrl" />
-      <Text ellipsis typography="paragraph-3-regular" mode="secondary">{{ article.domain }}</Text>
-    </Stack>
-  </Card>
+  <div ref="targetRef">
+    <Card
+      :background="articleStore.selectedArticle?.id === props.article.id ? 'secondary' : 'default'"
+      interactive
+      stretched
+      :onClick="props.onClick"
+    >
+      <Text ellipsis typography="title-4-semibold">{{ article.name }}</Text>
+      <Gap direction="vertical" :default="1" />
+      <Text ellipsis :clamp="2" typography="paragraph-3-regular">
+        <HighlightText :text="searchResultText || article.excerpt" :searchQuery="searchQuery" />
+      </Text>
+      <Gap direction="vertical" :default="2" />
+      <Stack direction="row" :gap="2" stretched>
+        <Favicon v-if="article.faviconUrl" :src="article.faviconUrl" />
+        <Text ellipsis typography="paragraph-3-regular" mode="secondary">{{ article.domain }}</Text>
+      </Stack>
+    </Card>
+  </div>
+  <ArticleCardContextMenu :data="contextMenuData" :article="ref(article)" />
 </template>
---
+++ src/web/components/ArticleCardContextMenu.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ContextMenu, ContextMenuList, ContextMenuItem, Icon } from 'gui'
+import { useArticleActions } from 'src/web/hooks/useArticleActions'
+
+export interface ArticleCardContextMenuProps {
+  article: Ref<Article>
+}
+
+const props = defineProps<ArticleCardContextMenuProps>()
+
+const { actions } = useArticleActions(props.article)
+</script>
+
+<template>
+  <ContextMenu v-bind="$attrs">
+    <ContextMenuList>
+      <ContextMenuItem v-for="action of actions" @click="action.actionHandler">
+        <Icon :name="action.icon" v-if="action.icon" />
+        {{ action.name }}
+      </ContextMenuItem>
+    </ContextMenuList>
+  </ContextMenu>
+</template>
+
+<style scoped></style>
