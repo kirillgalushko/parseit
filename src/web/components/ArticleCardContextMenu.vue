45ec6dea8b6efd5b5036d073ab318705720ad016
Kirill Galushko <yoursgk@mail.ru>
2024-11-04 00:33:47 +0600

    Fix add article modal types

--- src/web/components/ArticleCardContextMenu.vue
+++ src/web/components/ArticleCardContextMenu.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { ContextMenu, ContextMenuList, ContextMenuItem, Icon } from 'gui'
+import { ContextMenu, ContextMenuList, ContextMenuItem, Icon, useContextMenu } from 'gui'
 import { useArticleActions } from 'src/web/hooks/useArticleActions'
+import { Article } from 'src/web/types/Article'
+import { Ref } from 'vue'
 
 export interface ArticleCardContextMenuProps {
   article: Ref<Article>
+  data: ReturnType<typeof useContextMenu>['contextMenuData']
 }
 
 const props = defineProps<ArticleCardContextMenuProps>()
@@ -12,7 +15,7 @@ const { actions } = useArticleActions(props.article)
 </script>
 
 <template>
-  <ContextMenu v-bind="$attrs">
+  <ContextMenu v-bind="$attrs" :data="props.data">
     <ContextMenuList>
       <ContextMenuItem v-for="action of actions" @click="action.actionHandler">
         <Icon :name="action.icon" v-if="action.icon" />
