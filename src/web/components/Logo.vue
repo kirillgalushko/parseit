b2ea819761605ea7ed23aa90ff49451b24f518f3
Kirill Galushko <yoursgk@mail.ru>
2024-11-05 14:25:44 +0600

    Fix logo in light theme

--- src/web/components/Logo.vue
+++ src/web/components/Logo.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import logoCompact from 'src/web/assets/logo-white-clip.svg'
-import logoLong from 'src/web/assets/logo-white-long.svg'
+import logoCompact from 'src/web/assets/logo-white-clip.svg?raw'
+import logoLong from 'src/web/assets/logo-white-long.svg?raw'
 import { inject, Ref } from 'vue'
 
 const isCompact = inject<Ref<boolean>>('sidebar-is-compact')
 </script>
 
 <template>
-  <div :class="['logo', { compact: isCompact }]">
-    <img :src="isCompact ? logoCompact : logoLong" alt="Logo" />
-  </div>
+  <div :class="['logo', { compact: isCompact }]" v-html="isCompact ? logoCompact : logoLong"></div>
 </template>
 
 <style scoped>
 .logo {
   display: inline-flex;
+  color: hsl(var(--foreground));
 }
 
 .logo.compact {
