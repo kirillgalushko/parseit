67ce7125c05252e977a1d9fdb06faa57abdd2df8
Kirill Galushko <yoursgk@mail.ru>
2024-11-06 00:51:36 +0600

    Fix vite config for web dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src')
+    }
+  }
 })
