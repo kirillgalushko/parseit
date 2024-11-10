bc5ac6a8c4a0ddd899c9dd2fb52b0eb0d2574c7d
Kirill Galushko <yoursgk@mail.ru>
2024-11-10 22:21:25 +0600

    Fix vite base path for demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,26 @@ import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      src: resolve(__dirname, 'src')
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'chunks/[name]-[hash].js',
-        entryFileNames: '[name].js',
-        assetFileNames: '[name]-[hash][extname]',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+export default defineConfig(() => {
+  const basePath = process.env.VITE_DEMO_MODE === 'true' ? '/parseit/' : '/'
+  return {
+    base: basePath,
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        src: resolve(__dirname, 'src')
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'chunks/[name]-[hash].js',
+          entryFileNames: '[name].js',
+          assetFileNames: '[name]-[hash][extname]',
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString()
+            }
           }
         }
       }
